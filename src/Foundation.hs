72cce2be8c820326d1d0d5050ac0f5f92b494bc9
Tomas Stenlund <tomas.stenlund@telia.com>
2017-11-11 22:38:51 +0100

    Fixed to bootstrap4

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -139,7 +139,10 @@ instance Yesod App where
         -- you to use normal widget features in default-layout.
 
         pc <- widgetToPageContent $ do
-            addStylesheet $ StaticR css_bootstrap_css
+            addStylesheetRemoteAttrs "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
+              [("integrity","sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb"),
+               ("crossorigin", "anonymous")]
+            -- addStylesheet $ StaticR css_bootstrap_css
             $(widgetFile "default-layout")
         withUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
 
